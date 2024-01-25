ba8d943b55e5cbd32130c76c4f08e8f24d1ccb57
Micha <michael.rigas@t-online.de>
2024-01-25 14:18:04 +0100

    change

---
+++ Aufgabe1/aufgabe1.js
@@ -0,0 +1,10 @@
+function aufgabe1(inputString) {
+    if (typeof inputString !== 'string') {
+        console.error("Fehler: Das Argument muss ein String sein.");
+        return;
+    }
+
+    let reversedString = inputString.split('').reverse().join('');
+    console.log(reversedString);
+}
+
